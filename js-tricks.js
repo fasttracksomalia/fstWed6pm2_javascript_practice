c2c14174e9123eac2692434d403e7e44d76e103b
= <=>
2023-11-08 19:54:40 +0000

    added js-tricks.js file, operators and modified functions

---
+++ js-tricks.js
@@ -0,0 +1,12 @@
+
+// what is the output of the code below
+let age = 20;
+age = + 5;
+
+console.log(age)
+
+// what is the output of the code below
+
+let numOfPens = 20;
+let sum = numOfPens / 0
+console.log(sum)
